d0556ca2230ab8ffa716eae1764b429aec96542c
Ollie <code@ollie.codes>
2023-11-24 11:00:56 +0000

    feat(provider): Add an Eloquent based tenant provider

---
+++ src/Core/Providers/BaseTenantProvider.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Tenanted\Core\Providers;
+
+use Tenanted\Core\Contracts\TenantProvider;
+
+/**
+ * Base Tenant Provider
+ *
+ * This tenant provider provides a shared base for all other tenant providers.
+ */
+abstract class BaseTenantProvider implements TenantProvider
+{
+    /**
+     * The tenant provider name
+     *
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * Create a new base tenant provider instance
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     *
+     * Get the name of the tenant provider
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
---
+++ src/Core/Providers/EloquentTenantProvider.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace Tenanted\Core\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Tenanted\Core\Contracts\Tenant;
+
+/**
+ * Eloquent Tenant Provider
+ *
+ * An implementation of the {@see \Tenanted\Core\Contracts\TenantProvider}
+ * contract providing support for Eloquent models that are Tenants.
+ */
+class EloquentTenantProvider extends BaseTenantProvider
+{
+    /**
+     * The Eloquent tenant model
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant>
+     */
+    private string $model;
+
+    /**
+     * Create a new eloquent user provider
+     *
+     * @param string          $name
+     * @param class-string<\Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant> $model
+     */
+    public function __construct(string $name, string $model)
+    {
+        parent::__construct($name);
+        $this->model = $model;
+    }
+
+    /**
+     * Get the model class
+     *
+     * @return class-string<\Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant>
+     */
+    public function getModelClass(): string
+    {
+        return $this->model;
+    }
+
+    /**
+     * Get the model
+     *
+     * @return \Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant
+     */
+    public function getModel(): Model
+    {
+        /** @psalm-suppress UnsafeInstantiation */
+        return new $this->model;
+    }
+
+    /**
+     * @param string $identifier
+     *
+     * @return (\Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant)|null
+     */
+    public function retrieveByIdentifier(string $identifier): ?Tenant
+    {
+        $model = $this->getModel();
+
+        /**
+         * We have to disable this inspection because of the horrible return
+         * types in the Laravel query builder.
+         *
+         * @noinspection PhpIncompatibleReturnTypeInspection
+         * @noinspection UnknownInspectionInspection
+         */
+        return $model->newQuery()
+                     ->where($model->getTenantIdentifierName(), '=', $identifier)
+                     ->first();
+    }
+
+    /**
+     * @param int|string $key
+     *
+     * @return (\Illuminate\Database\Eloquent\Model&\Tenanted\Core\Contracts\Tenant)|null
+     */
+    public function retrieveByKey(int|string $key): ?Tenant
+    {
+        $model = $this->getModel();
+
+        /**
+         * We have to disable this inspection because of the horrible return
+         * types in the Laravel query builder.
+         *
+         * @noinspection PhpIncompatibleReturnTypeInspection
+         * @noinspection UnknownInspectionInspection
+         */
+        return $model->newQuery()
+                     ->where($model->getTenantKeyName(), '=', $key)
+                     ->first();
+    }
+}
