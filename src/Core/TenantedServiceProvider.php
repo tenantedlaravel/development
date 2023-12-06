60f80fd128e246c62937d168599909886eab4865
Ollie <code@ollie.codes>
2023-12-06 13:46:12 +0000

    chore: Register the middleware in the service provider

--- src/Core/TenantedServiceProvider.php
+++ src/Core/TenantedServiceProvider.php
@@ -4,18 +4,34 @@ declare(strict_types=1);
 namespace Tenanted\Core;
 
 use Illuminate\Support\ServiceProvider;
+use Tenanted\Core\Http\Middleware\IdentifyTenant;
+use Tenanted\Core\Http\Middleware\SetTenantHeader;
 
 class TenantedServiceProvider extends ServiceProvider
 {
+    /**
+     * Boots the tenanted package
+     *
+     * @return void
+     */
     public function boot(): void
     {
         // Publish the config
         $this->publishes([__DIR__ . '/../../config/tenanted.php' => config_path('tenanted.php')]);
     }
 
+    /**
+     * Register the tenanted package
+     *
+     * @return void
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function register(): void
     {
         $this->registerManager();
+        $this->registerMiddleware();
     }
 
     /**
@@ -30,6 +46,28 @@ class TenantedServiceProvider extends ServiceProvider
     {
         $this->app->bind(TenantedManager::class, function () {
             return new TenantedManager($this->app);
-        }, true);
+        },               true);
+    }
+
+    /**
+     * Register the package middleware
+     *
+     * @return void
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    private function registerMiddleware(): void
+    {
+        /**
+         * @var \Illuminate\Routing\Router $router
+         */
+        $router = $this->app->get('router');
+
+        // Create an alias for the identification middleware
+        $router->aliasMiddleware('tenanted.identify', IdentifyTenant::class);
+
+        // Create an alias for the header middleware
+        $router->aliasMiddleware('tenanted.header', SetTenantHeader::class);
     }
 }
